0120616236a9c86532281d4b4491991a8b2bc8fd
Joel H. <143587522+Joelh123@users.noreply.github.com>
2024-09-11 13:17:15 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+function muunnaLampotila() {
+    let muunnettava = document.getElementById("lampotila").value;
+    if (isNaN(muunnettava) || muunnettava == "" || muunnettava <= -273.15) {
+        document.getElementById("virhe").innerHTML = "Virheellinen syöte"
+        return
+    } else {
+        document.getElementById("virhe").innerHTML = ""
+    }
+    const muunnos = document.getElementById("yksikko").value;
+    const valittuDesimaali = document.querySelector('input[name="desimaalit"]:checked').value;
+    let celciusFahrenheit = (muunnettava) * 1.8 + 32
+    let fahrenheitCelcius =  (muunnettava - 32) / 1.8
+
+    if (muunnos == "c-to-f") {
+        let pyoristetty = Math.round(celciusFahrenheit * 10) / 10
+        const fixed = pyoristetty.toFixed(valittuDesimaali)
+        document.getElementById("tulos-print").innerHTML = `${muunnettava} Celcius astetta on:`
+        document.getElementById("palautus").innerHTML = `${fixed}°F` 
+    } 
+
+    if (muunnos == "f-to-c") {
+        let pyoristetty = Math.round(fahrenheitCelcius * 10) / 10
+        const fixed = pyoristetty.toFixed(valittuDesimaali)
+        document.getElementById("tulos-print").innerHTML = `${muunnettava} Fahrenheit astetta on:`
+        document.getElementById("palautus").innerHTML = `${fixed}°C` 
+    }
+}
